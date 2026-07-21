45d02cc6914e8a17eaaba3d86537fa8a1aa0d788
Shubham Giri <shubhammgiri3@gmail.com>
2026-07-21 22:55:15 +0530

    feat: add basic server with express

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, { type Request, type Response } from "express"
+
+const PORT = process.env.PORT || 3000;
+
+const app = express()
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello express | typescript | postgresSQL")
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+})
