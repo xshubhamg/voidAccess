9ed9bed253ac939de73cd8be5bf1bf3f708ad080
Shubham Giri <shubhammgiri3@gmail.com>
2026-07-21 23:37:47 +0530

    chore: add linting and formatting configs

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,13 @@
-import express, { type Request, type Response } from "express"
+import express, { type Request, type Response } from "express";
 
 const PORT = process.env.PORT || 3000;
 
-const app = express()
+const app = express();
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello express | typescript | postgresSQL")
-})
-
+  res.send("Hello express | typescript | postgresSQL");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
-})
+});
